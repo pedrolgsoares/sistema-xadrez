b85af07485cccb84b4603a57fbcc00b9533d0ff5
Pedro Lucas <pedrodevmob@gmail.com>
2022-05-18 21:26:16 -0300

    Implementado UI interface

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -27,4 +27,11 @@ public class Board {
     public void setColums(int colums) {
         this.colums = colums;
     }
+
+    public Piece piece(int row, int colum){
+        return pieces[row][colum];
+    }
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColumn()];
+    }
 }
